396935f98d043680c2ca297924bcad79962ad1ba
TheElephantCoder <157982101+TheElephantCoder@users.noreply.github.com>
2024-11-18 17:55:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.getElementById("run-button").addEventListener("click", function() {
   const contentElement = document.getElementById("Content");
-  contentElement.textContent = "...";
+  contentElement.textContent = "Running...";
 
   // Simulate project output
   setTimeout(() => {
